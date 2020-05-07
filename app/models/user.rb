9109e1356f0c7931bbe1f977b47f77402b5e6a27
Kevin Finn <superkevin@gmail.com>
2020-05-07 18:03:21 -0400

    fix login bug for guest users with existing games

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable
 
   has_many :players
   has_many :games, through: :players
