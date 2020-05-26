e088cd73e352a68ad7f6b1bd940514d9839d90d0
Kevin Finn <superkevin@gmail.com>
2020-05-25 23:47:03 -0400

    dynamic board size

--- app/javascript/components/BoardSvg.tsx
+++ app/javascript/components/BoardSvg.tsx
@@ -4,16 +4,39 @@ import { Game } from '../models/Game';
 import BorderSvg from './BorderSvg';
 import CornerSvg from './CornerSvg';
 import HarborSvg from './HarborSvg';
-import TerritorySvg from './TerritorySvg';
+import TerritorySvg, { territoryCenterX, territoryCenterY, TERRITORY_RADIUS } from './TerritorySvg';
 
 export interface BoardSvgProps {
     game: Game
 }
 
+const BOARD_MARGIN = 105
+
 export default function BoardSvg({ game }: BoardSvgProps) {
+    const { territories } = game
+    const territoryXs = _.map(territories, 'x')
+    const territoryYs = _.map(territories, 'y')
+
+    const minTerritoryX = _.min(territoryXs)
+    const maxTerritoryX = _.max(territoryXs)
+    const minTerritoryY = _.min(territoryYs)
+    const maxTerritoryY = _.max(territoryYs)
+
+    const widthBetweenTerritoryCenters = territoryCenterX({ x: maxTerritoryX, y: 0}) - territoryCenterX({ x: minTerritoryX, y: 0 })
+    const heightBetweenTerritoryCenters = territoryCenterY({ x: 0, y: maxTerritoryY }) - territoryCenterY({ x: 0, y: minTerritoryY })
+
+    const widthOfTerritories = widthBetweenTerritoryCenters + TERRITORY_RADIUS * 2
+    const heightOfTerritories = heightBetweenTerritoryCenters + TERRITORY_RADIUS * 2
+
+    const width = widthOfTerritories + BOARD_MARGIN
+    const height = heightOfTerritories + BOARD_MARGIN
+
+    const x = -width / 2
+    const y = -height / 2
+
     return (
         <React.Fragment>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="-235 -245 515 500" className="board">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox={`${x} ${y} ${width} ${height}`} className="board">
                 {
                     _.map(game.harbors, (harbor) => <HarborSvg key={harbor.id} harbor={harbor} />)
                 }
